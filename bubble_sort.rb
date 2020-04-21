f25bd816fc3f7bc142fc33163d33582321e7485b
erkeen <63852123+erkeen@users.noreply.github.com>
2020-04-21 23:27:31 +0300

    fixed linter errors

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,16 +1,16 @@
-def bubble_sort my_array
-    n = my_array.length
-    swap = true
-    while swap
-        swap = false
-            (n - 1).times do |index|
-                if my_array[index] > my_array[index + 1]
-                    my_array[index], my_array[index + 1] = my_array[index + 1], my_array[index]
-                    swap = true
-                end
-            end
-    end
-    return my_array
+def bubble_sort(my_array)
+  n = my_array.length
+  swap = true
+  while swap
+    swap = false
+      (n - 1).times do |index|
+        if my_array[index] > my_array[index + 1]
+          my_array[index], my_array[index + 1] = my_array[index + 1], my_array[index]
+          swap = true
+        end
+      end
+  end
+  return my_array
 end
 
 puts bubble_sort([4, 3, 78, 2, 0, 2])    
